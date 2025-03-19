e2db6f8622adba5c16a5bd3a17bdf11cc84cdce8
Ashadeep Majhi <asadipmajhi07@gmail.com>
2025-03-19 10:40:07 +0530

    E-commerce-React

---
+++ src/assets/mockData.jsx
@@ -0,0 +1,153 @@
+import Image1 from "./Images/top1.jpg";
+import Image2 from "./Images/top2.jpg";
+import Image3 from "./Images/top3.jpg";
+import Image4 from "./Images/top4.jpg";
+import Image5 from "./Images/top5.jpg";
+import Image6 from "./Images/shop1.jpg";
+import Image7 from "./Images/shop2.jpg";
+import Image8 from "./Images/shop3.jpg";
+import Image9 from "./Images/shop4.jpg";
+import Image10 from "./Images/shop5.jpg";
+import Image11 from "./Images/shop6.jpg";
+import Image12 from "./Images/shop7.jpg";
+import Image13 from "./Images/shop8.jpg";
+import Image14 from "./Images/shop9.jpg";
+import Image15 from "./Images/shop10.jpg";
+import Image16 from "./Images/shop11.jpg";
+import Image17 from "./Images/shop12.jpg";
+import Image18 from "./Images/shop13.jpg";
+import Image19 from "./Images/shop14.jpg";
+import Image20 from "./Images/shop15.jpg";
+
+export const Categories = [
+    "Electronics",
+    "Fashion",
+    "Home & Kitchen",
+    "Beauty",
+    "Sports",
+    "Automotive"
+];
+
+
+export const mockData = [
+    {
+        id: 1,
+        Image: Image1,
+        name: "bagpacks",
+        price: 999.99,
+    },
+    {
+        id: 2,
+        Image: Image2,
+        name: "Watch",
+        price: 887.99,
+    },
+    {
+        id: 3,
+        Image: Image3,
+        name: "headphone",
+        price: 1009.99,
+    },
+    {
+        id: 4,
+        Image: Image4,
+        name: "T-shirt",
+        price: 294.99,
+    },
+    {
+        id: 5,
+        Image: Image5,
+        name: "Sponsored",
+        price: 199.99,
+    },
+    {
+        id: 6,
+        Image: Image6,
+        name: "Shoes",
+        price: 2699.99,
+    },
+    {
+        id: 7,
+        Image: Image7,
+        name: "ph cover",
+        price: 199.99,
+    },
+    {
+        id: 8,
+        Image: Image8,
+        name: "phone",
+        price: 55000.99,
+    },
+    {
+        id: 9,
+        Image: Image9,
+        name: "laptop",
+        price: 89000.99,
+    },
+    {
+        id: 10,
+        Image: Image10,
+        name: "Cargos",
+        price: 1099.99,
+    },
+    {
+        id: 11,
+        Image: Image11,
+        name: "jeans",
+        price: 899.99,
+    },
+    {
+        id: 12,
+        Image: Image12,
+        name: "hoodie",
+        price: 413.99,
+    },
+    {
+        id: 13,
+        Image: Image13,
+        name: "Sandals",
+        price: 796.99,
+    },
+    {
+        id: 14,
+        Image: Image14,
+        name: "Perfume",
+        price: 533.99,
+    },
+    {
+        id: 15,
+        Image: Image15,
+        name: "Curtains",
+        price: 699.99,
+    },
+    {
+        id: 16,
+        Image: Image16,
+        name: "Shirt",
+        price: 417.99,
+    },
+    {
+        id: 17,
+        Image: Image17,
+        name: "Earbuds",
+        price: 1099.99,
+    },
+    {
+        id: 18,
+        Image: Image18,
+        name: "Speaker",
+        price: 1834.99,
+    },
+    {
+        id: 19,
+        Image: Image19,
+        name: "Smart Watch",
+        price: 1099.99,
+    },
+    {
+        id: 20,
+        Image: Image20,
+        name: "Sandals",
+        price: 399.99,
+    },
+];
